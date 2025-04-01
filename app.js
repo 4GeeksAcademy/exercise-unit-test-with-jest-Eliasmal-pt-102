f5ec5b38237f346fb0a62d2ff190d8b290da4e0a
Eliasmal Rodriguez <Eliasmal0228@gmail.com>
2025-04-01 19:20:33 +0000

    agregando test faltantes

---
+++ app.js
@@ -0,0 +1,27 @@
+const sum = (a,b) => {
+    return a + b
+}
+
+// Solo un registro en consola para nosotros
+console.log(sum(7,3))
+
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+const fromDollartoYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 156.5;
+    return valueInYen;
+}
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.87;
+    return valueInPound;
+}
+
+module.exports = { sum, fromEuroToDollar,fromDollartoYen, fromYenToPound };
